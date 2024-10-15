19471d45ba3f3c7af973d652822ac2effd5c45de
JohnnyCaringi <JohnnyCaringi9@gmail.com>
2024-10-15 11:35:49 -0500

    Mostly completed classifyQueue function

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ const { ObjectId } = require('mongodb')
 const { MongoClient } = require("mongodb");
 
 const uri = process.env.MONGO_URI;
-
 //Load Database
 const client = new MongoClient(uri);
-async function run() {
+
+async function loadTickets() {
     try {
         const database = client.db('ai-project');
         const tickets = database.collection('tickets');
-        // Query for a movie that has the title 'Back to the Future'
+        
         let ticket = await tickets.find({}).toArray();
+
         return ticket;
         //console.log(ticket);
       } finally {
@@ -20,38 +21,39 @@ async function run() {
       }
 }
 
-run().then(tickets => {
-    console.log(tickets);
-});
+async function classifyQueue(ticketText){
+  // Train Model
+  // Always classify a document even if teh model is unsure
+  const manager = new NlpManager({ languages: ['en'], nlu: { log: false } });
 
+  queues = [];
 
+  // Loop through all tickets and add them to the model
+  loadTickets().then(tickets => {
+    for (let i = 0; i < tickets.length; i++){
+      manager.addDocument(tickets[i].language, tickets[i].subject + " " + tickets[i].text, tickets[i].queue);
 
-//Train Model
-// Always classify a document even if teh model is unsure
-const manager = new NlpManager({ languages: ['en'], nlu: { useNoneFeature: false } });
+      // Allows for more queues to be added in the future
+      if(!queues.includes(tickets[i].queue)){
+        queues.push(tickets[i].queue);
+      }
+    }
 
-// Adds the utterances and intents for the NLP
-manager.addDocument('en', 'goodbye for now', 'greetings.bye');
-manager.addDocument('en', 'bye bye take care', 'greetings.bye');
-manager.addDocument('en', 'okay see you later', 'greetings.bye');
-manager.addDocument('en', 'bye for now', 'greetings.bye');
-manager.addDocument('en', 'i must go', 'greetings.bye');
-manager.addDocument('en', 'hello', 'greetings.hello');
-manager.addDocument('en', 'hi', 'greetings.hello');
-manager.addDocument('en', 'howdy', 'greetings.hello');
+    // Tell the model how to respond
+    for (let i = 0; i < queues.length; i++){
+      manager.addAnswer('en', queues[i], queues[i])
+    }
 
+    // Train the model
+    (async() => {
+      await manager.train();
+      manager.save();
+      const response = await manager.process('en', ticketText);
+      //return response.answer;
+      console.log(response);
+    })();
+  });
+}
 
-// Train also the NLG
-manager.addAnswer('en', 'greetings.bye', 'Till next time');
-manager.addAnswer('en', 'greetings.bye', 'see you soon!');
-manager.addAnswer('en', 'greetings.hello', 'Hey there!');
-manager.addAnswer('en', 'greetings.hello', 'Greetings!');
+classifyQueue("My photoshop keeps crashing even after I reinstall");
 
-
-// Train and save the model.
-(async() => {
-    await manager.train();
-    manager.save();
-    const response = await manager.process('en', "It is getting late");
-    console.log(response);
-})();
